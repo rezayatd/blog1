862692cc4a9a823e24fbae66a30eb2f024f8680e
rezayat <dariush.rezayat@cibc.com>
2023-01-02 16:04:51 -0500

    Jan 2 first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'blog1'`, () => {
+  it(`should have as title 'hello-form'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blog1');
+    expect(app.title).toEqual('hello-form');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('blog1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('hello-form app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { FeaturesComponent } from './components/home/features/features.component';
+import { PreviewComponent } from './components/home/preview/preview.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FeaturesComponent,
+    PreviewComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
